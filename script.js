2afbb97b62e7a550b9068231156718dc749a0f4e
Korch <stephaniekorch@quickenloans.com>
2021-06-10 21:38:53 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function getAreaOfCircle (radius) {
+    return Math.PI * radius**2;
+}
+
+function getCircumferenceOfCircle (radius) {
+    return 2 * radius * Math.PI;
+}
+
+function getAreaOfSquare (side) {
+    return side**2;
+}
+
+function getAreaOfTriangle (base, height) {
+    return 0.5 * base * height;
+}
